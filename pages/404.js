d7c7206ebcb503f9139972391e31460d082854f8
Gyuner Saliev <g.saliev@gmail.com>
2022-09-10 13:24:51 +0300

    404 image

--- pages/404.js
+++ pages/404.js
@@ -6,12 +6,12 @@ function NotFoundPage() {
     return (
         <div>
             <h1>Page not found</h1>
-            <Image
+            {/* <Image
           src='/images/site/404.jpg'
           alt='404 error page not found'
           width={860}
           height={640}
-        />
+        /> */}
         </div>
     );
     

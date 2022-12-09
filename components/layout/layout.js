f9c36683db94cde5adc5ac1d5ba365f2326b7e0f
mubashar <hmubashar639@gmail.com>
2022-12-09 12:23:21 +0500

    layout done

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,25 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { createContext, Fragment, useState } from "react";
 import Footer from "../footer/Footer";
 
 import MainNavigation from "./main-navigation";
-export const SearchContact = createContext("");
+export const SearchContact = createContext({});
 
 function Layout(props) {
-  const { query } = useRouter();
+  const { query, pathname, push } = useRouter();
   const [search, setSearch] = useState(query?.search);
+  useEffect(() => {
+    return () => {
+      if (search && pathname !== "/recipes") {
+        push("/recipes", {
+          query: {
+            ...query,
+          },
+        });
+      }
+    };
+  }, [search]);
 
   return (
     <SearchContact.Provider value={{ search, setSearch }}>
